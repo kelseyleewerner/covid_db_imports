fdba79587c805a9a0c45b8949493d3d84ec3a4cb
kelseyleewerner <kelsey.lee.werner@gmail.com>
2022-09-03 20:02:35 -0700

    move python import scripts to correct directory

--- vax_rel.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-countries = ['Chile', 'Iceland', 'Nigeria', 'China', 'United States', 'Australia', 'Botswana', 'South Korea', 'Uruguay', 'Russia', 'Yemen', 'Rwanda', 'Brazil', 'India', 'Croatia', 'Italy', 'Ecuador', 'Saudi Arabia', 'Germany', 'Egypt', 'Iraq', 'Hungary', 'France', 'Kenya', 'Austria']
-
-
-
-
